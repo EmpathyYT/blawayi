915c232cac568b4c26b1a784f07812191e1756d4
BlackWing <blackwingprograms@gmail.com>
2023-03-28 03:51:53 +0300

    fixed bugs in the chat window

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ let window;
 
 class Windows {
     win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1490,
+        height: 700,
         webPreferences: {
             nodeIntegration: true,
             preload: path.join(__dirname, 'preload.js'),
--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
 const toggleSidebarBtn = document.getElementById('toggle-sidebar');
 const sidebar = document.getElementById('sidebar');
 const chatList = document.getElementById("contact-list")
+let footerStat = false
 data = {}
 
 
@@ -13,6 +14,14 @@ function createButton() {
 
 toggleSidebarBtn.addEventListener('click', () => {
     sidebar.classList.toggle('open');
+    if (footerStat === true) {
+        try {
+            let footer = document.getElementById("chat-footer");
+            footer.classList.toggle("hidden")
+        } catch (err) {
+            console.log(err)
+        }
+    }
 });
 
 const contactButton = document.getElementById('contact-button');
@@ -57,14 +66,27 @@ window.electron.getChats((event) => {
 })
 
 function handling() {
+    sidebar.classList.toggle('open');
     let main = document.getElementById("main-screen")
     while (main.firstChild) main.firstChild.remove();
-    sidebar.classList.toggle('open');
     let title = document.createElement("h1");
+    let hr = document.createElement('hr')
+    hr.id = "chat-hr"
     title.innerHTML = "This is the beginning of your chat with me";
     main.appendChild(title);
-    main.appendChild(document.createElement('hr'))
-    //TODO: Message bar and auto scrolling
+    main.appendChild(hr)
+    let footerContainer = document.createElement('div')
+    let footerInput = document.createElement('input')
+    let footer = document.createElement('footer')
+    footerInput.type = "text"
+    footerInput.placeholder="Type your message here..."
+    footerContainer.className = "message-input-container"
+    footer.id = "chat-footer"
+    footerContainer.appendChild(footerInput)
+    footer.appendChild(footerContainer)
+    main.appendChild(footer)
+    footerStat = true;
+    //TODO: work on the chat
 
 }
 

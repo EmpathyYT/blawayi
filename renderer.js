8d671b732469f047d774877ddede5b21de15675e
BlackWing <blackwingprograms@gmail.com>
2023-03-29 03:11:41 +0300

    Added chat capabilities (it was hell) ((fuc css bro))

--- renderer.js
+++ renderer.js
@@ -3,13 +3,12 @@ const sidebar = document.getElementById('sidebar');
 const chatList = document.getElementById("contact-list")
 let footerStat = false
 data = {}
-
+let messages = {}
 
 
 function createButton() {
     return document.createElement("button");
 }
-
 
 
 toggleSidebarBtn.addEventListener('click', () => {
@@ -36,7 +35,9 @@ window.electron.loadChats((event, values) => {
         button.id = key
         button.innerHTML = `${value}`
         button.className = "button-list"
-        button.onclick = handling
+        button.onclick = () => {
+            handling(value)
+        }
         node.appendChild(button)
         chatList.appendChild(node)
     }
@@ -49,6 +50,9 @@ window.electron.onChatUpdate((event, values) => {
     button.id = values[1]
     button.innerHTML = values[0]
     button.className = "button-list"
+    button.onclick = () => {
+        handling(values[0])
+    }
     node.appendChild(button)
     chatList.appendChild(node)
 
@@ -65,21 +69,45 @@ window.electron.getChats((event) => {
 
 })
 
-function handling() {
+function handling(name) {
     sidebar.classList.toggle('open');
     let main = document.getElementById("main-screen")
     while (main.firstChild) main.firstChild.remove();
     let title = document.createElement("h1");
     let hr = document.createElement('hr')
+    let textArea = document.createElement('div')
+    let chatContainer = document.createElement("div")
+    chatContainer.id = "chat-container"
+    textArea.id = "text-area"
     hr.id = "chat-hr"
-    title.innerHTML = "This is the beginning of your chat with me";
+    title.innerHTML = `This is the beginning of your chat with ${name}`;
+    chatContainer.appendChild(textArea)
     main.appendChild(title);
     main.appendChild(hr)
+    main.appendChild(chatContainer)
     let footerContainer = document.createElement('div')
     let footerInput = document.createElement('input')
     let footer = document.createElement('footer')
     footerInput.type = "text"
-    footerInput.placeholder="Type your message here..."
+    footerInput.placeholder = "Type your message here..."
+    footerInput.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            if (event.target.value === " ") {
+                event.target.value = "";
+                return;
+            }
+            messages["main"] += event.target.value;
+            let message = document.createElement("div");
+            let messageText = document.createElement("p");
+            messageText.innerHTML = event.target.value;
+            message.className = "message";
+            message.appendChild(messageText);
+            textArea.appendChild(message);
+            window.scrollTo(0, document.body.scrollHeight);
+            event.target.value = "";
+        }
+    });
     footerContainer.className = "message-input-container"
     footer.id = "chat-footer"
     footerContainer.appendChild(footerInput)
